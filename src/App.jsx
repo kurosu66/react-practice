a3c95ec9910791749b688b9fe39007b9524319b9
kurosu66 <kurosu.66@gmail.com>
2026-08-10 23:05:46 +0900

    タスク削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,18 @@ export default function App() {
     }))
   }
 
+  function handleDeleteTasks(taskId) {
+    setTasks(tasks.filter(t =>
+      t.id !== taskId
+    ))
+  };
+
   return (
     <>
       <TaskList
         tasks={tasks}
         onToggle={handleToggleTasks}
+        onDelete={handleDeleteTasks}
       />
     </>
   );
