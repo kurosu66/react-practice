9fa6bc3e4258bc303bd0d389c4cc8212aed164ed
kurosu66 <kurosu.66@gmail.com>
2026-08-27 07:34:29 +0900

    タスク一覧表示

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import TaskList from './TaskList'
+import { useState } from 'react';
+
+export default function App() {
+  const initialTasks = [
+    { id: 1, title: '日本円を買う', completed: false },
+    { id: 2, title: '宿題', completed: false },
+    { id: 3, title: '掃除', completed: true },
+    { id: 4, title: '買い出し', completed: false }
+  ]
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  return (
+    <>
+      <TaskList tasks={tasks} />
+    </>
+  );
+}
